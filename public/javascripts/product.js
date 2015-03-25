3d800aa22f498285b198a6a616d753e0ed18f9a1
Charlie <gogogeorge123@yahoo.ca>
2015-03-25 17:09:03 -0400

    try

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -16,42 +16,39 @@ $(function(){
     }).selectable({
       filter: 'li div',
       selected: function(event,ui){
-
+        //add search query to 
       },
       selecting: function (event, ui) {
-          if ($(ui.selecting).parent().hasClass('level-1')) {
-              //this is a level-1 item, so select all of it's children
-              var ch = $(ui.selecting).parent().find('.level-2 .ui-selectee');
-              $(ch).not(".ui-selected").addClass("ui-selecting");
-          } else {
-              //this is a level-2 item, so check to see if all of it's siblings are also selected
-              var sibs = $(ui.selecting).parent().siblings().find('.ui-selectee');
-              var notSelected = 0;
-              for (var i = 0; i < sibs.length; i++) {
-                  if ($(sibs[i]).hasClass('ui-selected') || $(sibs[i]).hasClass('ui-selecting')) {
-                      notSelected = notSelected
-                  } else {
-                      notSelected = notSelected + 1;
-                  }
+        if ($(ui.selecting).parent().hasClass('level-1')) {
+          //this is a level-1 item, so select all of it's children
+          var ch = $(ui.selecting).parent().find('.level-2 .ui-selectee');
+          $(ch).not(".ui-selected").addClass("ui-selecting");
+        } else {
+          //this is a level-2 item, so check to see if all of it's siblings are also selected
+          var sibs = $(ui.selecting).parent().siblings().find('.ui-selectee');
+          var notSelected = 0;
+          for (var i = 0; i < sibs.length; i++) {
+              if ($(sibs[i]).hasClass('ui-selected') || $(sibs[i]).hasClass('ui-selecting')) {
+                  notSelected = notSelected
+              } else {
+                  notSelected = notSelected + 1;
               }
-              if (notSelected === 0) { //all siblings are selected, so select their level-1 parent as well
-                  $(ui.selecting).parent().parent().parent().find('>:first-child').not(".ui-selected").addClass("ui-selecting");
-              }
-              //otherwise, just select as usual
           }
+          if (notSelected === 0) { //all siblings are selected, so select their level-1 parent as well
+              $(ui.selecting).parent().parent().parent().find('>:first-child').not(".ui-selected").addClass("ui-selecting");
+          }
+          //otherwise, just select as usual
+        }
       },
       unselected: function (event, ui) {
-        //console.log(ui);
-        //$(ui.unselected).parent().each(function(d){console.log(ui)});
-        //$(this).removeClass('ui-selected').addClass('ui-unselected');
         if ($(ui.unselected).parent().hasClass('level-1')) {
-            //unselect all of it's children
-            $(ui.unselected).parent().children().each(function () {
-                $(this).find('.ui-selectee').removeClass('ui-selected').addClass('ui-unselected');
-            });
+          //unselect all of it's children
+          $(ui.unselected).parent().children().each(function () {
+              $(this).find('.ui-selectee').removeClass('ui-selected').addClass('ui-unselected');
+          });
         } else {
-            //this is a level-2 item, so we need to deselect its level-1 parent
-            $(ui.unselected).parents('li.level-1').find(">:first-child").removeClass('ui-selected');
+          //this is a level-2 item, so we need to deselect its level-1 parent
+          $(ui.unselected).parents('li.level-1').find(">:first-child").removeClass('ui-selected');
         }
       }
   });
