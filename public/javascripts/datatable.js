cc6b06736a5bafe7c15adf44b54a0c0edb172bc0
CharlyZ <gogogeorge123@yahoo.ca>
2015-03-29 14:27:48 -0400

    ready to maek server code for product

--- public/javascripts/datatable.js
+++ public/javascripts/datatable.js
@@ -31,23 +31,24 @@ $(document).ready(function() {
           var row = $(e.relatedTarget).data(); 
           if(row){//on update, else its on insert
             //$('select[name=cat_name]').val(2); // update the select
+            //$('.selectpicker').selectpicker('refresh');
           }
         });
     });
+    var validateEmpty = function(control){
 
+    }
+    //$('select[name=cat_name]').selectpicker(function(){});
 
-    $("#btnUpdate").click(function(){
-      var error = 0;
-      if($("#p_sku").val().length <= 0){
-
-      }
-      if($("#p_sku").val().length <= 0){
-
+    $("#btnUpdate").click(function(e){
+      e.stopPropagation();
+      var val = $('#cat_name').val();
+      if(val == 0){
+        alert("Don't forget to select a valid category");
       }
     });
 
     $("#btnDelete").click(function(){
-      
     });
 
   });
